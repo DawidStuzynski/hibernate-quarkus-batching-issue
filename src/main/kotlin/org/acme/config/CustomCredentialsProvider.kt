092763eb3b7bfffbb315acf5ce6f26621e96b6ca
Tincoquesse <kamil.sound@gmaIl.com>
2023-10-25 13:50:07 +0200

    Initial Commit

---
+++ src/main/kotlin/org/acme/config/CustomCredentialsProvider.kt
@@ -0,0 +1,19 @@
+package org.acme.config
+
+import io.quarkus.arc.Unremovable
+import io.quarkus.credentials.CredentialsProvider
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.inject.Named
+
+@Unremovable
+@ApplicationScoped
+@Named("custom-credentials-provider")
+class CustomCredentialsProvider : CredentialsProvider {
+
+    override fun getCredentials(credentialsProviderName: String?): Map<String?, String?>? {
+        val properties: MutableMap<String?, String?> = HashMap()
+        properties[CredentialsProvider.USER_PROPERTY_NAME] = "TEST_USERNAME"
+        properties[CredentialsProvider.PASSWORD_PROPERTY_NAME] = "TEST_PASSWORD"
+        return properties
+    }
+}
