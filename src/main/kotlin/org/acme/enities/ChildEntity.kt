5259ff8bd483979784e375aefcdf754e6547156d
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-11-02 08:19:02 +0100

    HypersistenceOptimizer null event

--- src/main/kotlin/org/acme/enities/ChildEntity.kt
+++ src/main/kotlin/org/acme/enities/ChildEntity.kt
@@ -1,6 +1,5 @@
 package org.acme.enities
 
-import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
@@ -9,7 +8,6 @@ import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.SequenceGenerator
-import jakarta.persistence.Table
 
 @Entity
 class ChildEntity(
